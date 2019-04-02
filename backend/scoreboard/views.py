c0c1f4f3ea12da3cf34516230c6fc7ac5ef9534c
Torin Foss <frost@Torins-MacBook-Pro.local>
2019-04-01 17:43:35 -0700

    compare by datetime succesful

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -7,22 +7,27 @@ Views for the scoreboard page
 from django.http import JsonResponse
 from .models import KattisHandle, KattisScore
 
-# from datetime import datetime, timedelta
-from django.utils import timezone
+from datetime import datetime, timedelta
+# from django.utils import timezone
 from django.db.models import Max, Min
 
+
+
 def test(request):
-	# obj = KattisHandle.objects.first()
 	return JsonResponse({"test":"test"}, safe=False)
 
 def scores_week(request):
 	# will have to update points to be the difference
 	# week_ago = timezone.timedelta(days = 7) - timezone.now()
-	week_ago = timezone.timedelta(days = -7) 
+	week_ago = datetime.now() - timedelta(days=7)
+	test = datetime.now() - timedelta(days=21)
+	# return JsonResponse({"time": week_ago}, safe=False)
+	# print(week_ago)
 	scores = map(
 		lambda obj:{
 			"id" : obj.id,
-			"score" : obj.score
+			"score" : obj.score, 
+			"date" : obj.created_at
 		# }, KattisScore.objects.all().order_by('score').filter(created_at__gte=week_ago))
-		}, KattisScore.objects.filter(id = 2))
+		}, KattisScore.objects.filter(created_at__gte = test))
 	return JsonResponse(list(scores), safe=False)
