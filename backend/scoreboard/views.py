4d7096093f81a7bd70c0758d46920135fe8acf83
Torin Foss <frost@Torins-MacBook-Pro.local>
2019-04-01 18:07:11 -0700

    annotating with min and max aggregate functions working

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -23,11 +23,14 @@ def scores_week(request):
 	test = datetime.now() - timedelta(days=21)
 	# return JsonResponse({"time": week_ago}, safe=False)
 	# print(week_ago)
-	scores = map(
-		lambda obj:{
-			"id" : obj.id,
-			"score" : obj.score, 
-			"date" : obj.created_at
-		# }, KattisScore.objects.all().order_by('score').filter(created_at__gte=week_ago))
-		}, KattisScore.objects.filter(created_at__gte = test))
-	return JsonResponse(list(scores), safe=False)
+
+	test = KattisScore.objects.values("kattis_handle").annotate(min=Min('score')).annotate(max=Max('score'))
+
+	# scores = map(
+	# 	lambda obj:{
+	# 		"handle" : obj.kattis_handle,
+	# 		"score" : obj.score, 
+	# 		"date" : obj.created_at
+	# 	# }, KattisScore.objects.all().order_by('score').filter(created_at__gte=week_ago))
+	# 	}, KattisScore.objects.values("kattis_handle"))
+	return JsonResponse(list(test), safe=False)
